2b98325fa9471ce778e79ce5a8d300ad9059b27b
s3ykku <s3ykku@gmail.com>
2025-08-24 19:11:38 +0300

    Add files via upload

---
+++ system_configuration.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PATHFROM="~/Downloads/archlinux_ricing/s3ykku"
+PATHTO="~/.config"
+PATHTOSCRIPTS="~/.scripts"
+
+mkdir -p ${PATHTO}/fish
+
+mkdir -p ${PATHTO}/hypr
+
+mkdir -p ${PATHTO}/kitty
+
+mkdir -p ${PATHTO}/waybar
+
+mkdir -p ${PATHTOSCRIPTS}/waybar
+
+mkdir -p ${PATHTOSCRIPTS}/grim
+
+cp ${PATHFROM}/config/hypr/hyprland.conf ${PATHTO}/hypr/
+
+cp ${PATHFROM}/config/fish/config.fish ${PATHTO}/fish/
+
+cp ${PATHFROM}/config/waybar/config.jsonc ${PATHTO}/waybar/
+
+cp ${PATHFROM}/config/waybar/style.css ${PATHTO}/waybar/
+
+cp ${PATHFROM}/scripts/grim/screenshot_to_clipboard.sh ${PATHTOSCRIPTS}/grim/
+
+cp ${PATHFROM}/scripts/waybar/launch.sh ${PATHTOSCRIPTS}/waybar/
+
+chmod +x ${PATHTOSCRIPTS}/waybar/launch.sh
+
+chmod +x ${PATHTOSCRIPTS}/grim/screenshot_to_clipboard.sh
