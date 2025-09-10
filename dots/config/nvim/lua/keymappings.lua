2159871b8178153726732051e79d0053beaaf8bc
s3ykku <s3ykku@gmail.com>
2025-09-10 21:17:02 +0300

    Add files via upload

---
+++ dots/config/nvim/lua/keymappings.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+
+vim.api.nvim_set_keymap('n', '<leader>e', ':Neotree toggle <CR>', { noremap = true, silent = true } )
+vim.api.nvim_set_keymap('n', '<leader>tn', ':colorscheme tokyonight-storm <CR>', { noremap = true, silent = true } )
+vim.api.nvim_set_keymap('n', '<leader>cp', ':colorscheme catppuccin-macchiato <CR>', { noremap = true, silent = true } )
