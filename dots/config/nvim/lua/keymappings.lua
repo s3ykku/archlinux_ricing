ae63b8f9ef9cb17214f098784f0d2745106dcbc5
s3ykku <s3ykku@gmail.com>
2025-09-21 14:35:17 +0300

    Delete dots directory

--- dots/config/nvim/lua/keymappings.lua
+++
@@ -1,4 +0,0 @@
-vim.g.mapleader = " "
-
-vim.api.nvim_set_keymap('n', '<leader>e', ':Neotree toggle <CR>', { noremap = true, silent = true } )
-vim.api.nvim_set_keymap('n', '<leader>tn', ':colorscheme tokyonight-storm <CR>', { noremap = true, silent = true } )
